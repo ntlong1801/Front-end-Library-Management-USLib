2c716a79b16e06f9d44bce7214c4ce348546d2d4
20120352 <phuc9a134@gmail.com>
2023-06-27 18:26:12 +0700

    add making loan page

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,11 @@ import FrontLayout from '@/layouts/FrontLayout';
 import LoginPage from '@/pages/Login';
 import HomePage from '@/pages/Home';
 import RegisterPage from '@/pages/Register';
+import MakingLoanPage from '@/pages/MakingLoan';
 
 export const publicRoutes = [
 	{ path: '/login', element: LoginPage, layout: FrontLayout },
 	{ path: '/register', element: RegisterPage, layout: FrontLayout },
 	{ path: '/', element: HomePage, layout: DefaultLayout },
+	{ path: '/makingloan', element: MakingLoanPage, layout: DefaultLayout },
 ];
